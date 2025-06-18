78b8aeea73c724dca356c82b38907c6f111a8dd8
zalikhaAZ <zalikhazainudin@gmail.com>
2025-06-18 05:51:42 +0000

    modify code

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,12 @@
 // Learning C++ 
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+#include <iostream>
+
+int main(){
+    std::cout <<"Hi There!" <<std::endl;
+    std::cout <<"How are you?" <<std::endl;
+
+    std::cout << std::endl << std::endl;
+    return(0);
+}
