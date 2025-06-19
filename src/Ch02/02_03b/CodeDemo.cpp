1a5d81a5681a7603d0e5880a29e7e662a1221123
zalikhaAZ <zalikhazainudin@gmail.com>
2025-06-19 07:39:01 +0000

    modified

--- src/Ch02/02_03b/CodeDemo.cpp
+++ src/Ch02/02_03b/CodeDemo.cpp
@@ -4,8 +4,28 @@
 
 #include <iostream>
 
+int a, b = 5; //single line comment
+
+/*
+multi 
+line
+comment
+*/
+
 int main(){
-    std::cout << "Hi There!" << std::endl;
+    bool my_flag;
+    a = 8;
+    my_flag = false;
+    std::cout << "a = " << a << std::endl;
+    std::cout << "b = " << b << std::endl;
+    std::cout << "flag = " << my_flag << std::endl;
+    std::cout << "unflag = " << my_flag + 1 << std::endl;
+    std::cout << "a + b = " << a + b << std::endl;
+    std::cout << "a - b = " << a - b << std::endl;
+    std:: cout << "b - a = " << b - a << std::endl;
+    unsigned int positive;
+    positive = b - a;
+    std::cout << "b - a (unsigned) = " << positive << std::endl;
     
     std::cout << std::endl << std::endl;
     return (0);
